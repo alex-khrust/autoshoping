ef8f6597640b10248a884fecabf9a32b37ac5e00
alex-khrust <st.alex.ser@gmail.com>
2018-11-20 02:05:09 +0200

    добавил переключение между табами

--- js/main.js
+++ js/main.js
@@ -13,39 +13,33 @@ $(document).ready(function ()  {
 //   })();
   
   // Добавление класса active пунктам меню --------------------------------
-  $('.block__menu a').click(function(){
-    $(".block__menu a").removeClass('active');
-    $(this).addClass('active');
-  });
   $('.type li a').click(function(){
     $(".type li a").removeClass('active');
     $(this).addClass('active');
   });
 //-------------------------------------------------------------------------
+// Переключение между табами, добавление класса active и скрытие контента ----------
+  $(".tab_content").hide();
+  $(".tab_content:first").show();
+  /* в режиме вкладок */
+  $(".block__menu a").click(function () {
+    $(".tab_content").hide();
+    var activeTab = $(this).attr("rel");
+    $("#" + activeTab).fadeIn();
+    $(".block__menu a").removeClass("active");
+    $(this).addClass("active");
+  });
+//-------------------------------------------------------------------------
 //Добавление и удаление классов по ширене экрана  ----------
-  var windowWidth = $(window).width();
-  if (windowWidth < 1070) $("header").addClass("mob-menu");
-  else $("header").removeClass("mob-menu");
-  
-  $(window).resize(function () {
-    var windowWidth = $(window).width();
-    if (windowWidth < 1070) $("header").addClass("mob-menu");
-    else $("header").removeClass("mob-menu");
-  });
-//----------------------------------------------------------
-// Animated Counter Bootstrap ------------------------------
-  $('.num').each(function () {
-    $(this).prop('Counter',0).animate({
-      Counter: $(this).text()
-    }, {
-      duration: 5000,
-      easing: 'swing',
-      step: function (now) {
-        $(this).text(Math.ceil(now));
-      }
-    });
-  });
-
+//   var windowWidth = $(window).width();
+//   if (windowWidth < 1070) $("header").addClass("mob-menu");
+//   else $("header").removeClass("mob-menu");
+//
+//   $(window).resize(function () {
+//     var windowWidth = $(window).width();
+//     if (windowWidth < 1070) $("header").addClass("mob-menu");
+//     else $("header").removeClass("mob-menu");
+//   });
 //----------------------------------------------------------
 
 
